245183cb82e16c9159c2237aedfc885d75338eae
Alejandro Rojas <kmachappy@pm.me>
2022-05-21 15:32:09 -0700

    frontend complete?

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
-export default function Projects(){
+export default function Projects({url}){
+    const [projects,setProjects] = useState(null)
 
 
-    return  <h1>this is the porject page </h1>
+
+    async function getProjects(){
+        const data =  await fetch(`${url}projects`).then(res=>res.json())
+        setProjects(data)
+    }
+
+    console.log(projects)
+    useEffect(()=>getProjects(),[])
+
+    function loaded(){
+        return(
+            <div className='project-container'>
+                {projects.map((e,i)=>{
+                    return(
+                        <div key={i} className='project-card'>
+                            <h1>{e.name}</h1>
+                            <img src={e.image} />
+                            <a href={e.git}>
+                                <button>GitHub</button>
+                            </a>
+                            <a href={e.live}>
+                                <button>Live Website</button>
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    return  projects ? loaded() : <h1>loaded....</h1>
 }
